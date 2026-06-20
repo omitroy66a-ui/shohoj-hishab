4fac1e17642ce795c42aeb7f1e961b1cab27c933
omitroy66a-ui <omitroy6.6.a@gmail.com>
2026-06-20 10:13:01 +0600

    Remove old JS files

--- frontend/src/App.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-function App() {
-    return _jsx("h1", { children: "Shohoj Hishab Working \u2705" });
-}
-export default App;
--- frontend/src/components/common/Layout.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import Header from './Header';
-import Footer from './Footer';
-import './Layout.css';
-const Layout = ({ children }) => {
-    return (_jsxs("div", { className: "layout", children: [_jsx(Header, {}), _jsx("main", { className: "main-content", children: children }), _jsx(Footer, {})] }));
-};
-export default Layout;
